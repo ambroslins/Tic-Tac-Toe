373ff370e28cb22c2676c72db2e01525f5f44d28
ambroslins <ambroslins@users.noreply.github.com>
2019-01-10 20:57:50 +0100

    draw board

--- main.c
+++ main.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
+#include <string.h>
+
+void drawBoard(const char *board) {
+    if(board == NULL || strlen(board) < 9) return;
+    printf(" %c | %c | %c \n", board[0], board[1], board[2]);
+    printf("-----------\n");
+    printf(" %c | %c | %c \n", board[3], board[4], board[5]);
+    printf("-----------\n");
+    printf(" %c | %c | %c \n", board[6], board[7], board[8]);
+}
 
 int main() {
-    printf("Hello, World!\n");
+    drawBoard("123456789");
+        
     return 0;
 }
